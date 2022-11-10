5b53d5c7b3436536151c519e4fc7835dd574ffa2
Sergey Zhuk <sergey.zhuk@heartex.com>
2022-11-10 20:47:17 +0000

    Add info to delete bucket

--- terraform/aws/scripts/destroy.sh
+++ terraform/aws/scripts/destroy.sh
@@ -34,15 +34,6 @@ terraform destroy -no-color -auto-approve ${TF_PARAMS}
 terraform workspace select default
 terraform workspace delete "${TF_VAR_environment:-}"
 
-echo -e "[INFO] Terraform state bucket should be deleted manually."
-echo -e "[INFO] Run these commands to completely remove bucket and all versioned state files"
-# TODO replace with informal message
-
-# Cleanup state bucket
-#ITEMS=$(aws s3api list-object-versions --bucket "${BUCKET_NAME}" --region "${TF_VAR_region:-}" --output json | jq '. | to_entries | [.[] | select(.key | match("^Versions|DeleteMarkers$")) | .value[] | {Key:.Key,VersionId:.VersionId}]')
-#
-#if [ -n "${ITEMS}" ]; then
-#  aws s3api delete-objects --bucket "${BUCKET_NAME}" --region "${TF_VAR_region:-}" --delete "{\"Objects\": ${ITEMS}}" >/dev/null
-#fi
-#
-#aws s3api delete-bucket --bucket "${BUCKET_NAME}" --region "${TF_VAR_region:-}" >/dev/null
+echo -e "\n[INFO] Terraform state bucket should be deleted manually."
+echo -e "[INFO] Run these commands to completely remove bucket and all versioned state files:\n"
+echo "aws s3api delete-objects --bucket ${BUCKET_NAME} --region ${TF_VAR_region:-} --delete \"{\\\"Objects\\\": \$(aws s3api list-object-versions --bucket ${BUCKET_NAME} --region ${TF_VAR_region:-} --output json | jq -c '. | to_entries | [.[] | select(.key | match(\"^Versions|DeleteMarkers$\")) | .value[] | {Key:.Key,VersionId:.VersionId}]')}\""
