153039a37574c77d12b170cff420dd2c98306aae
Nikita Belonogov <nikita.belonogov@heartex.com>
2022-12-08 19:12:07 +0400

    Add k8s secret encription

--- terraform/aws/modules/eks/main.tf
+++ terraform/aws/modules/eks/main.tf
@@ -1,15 +1,29 @@
 # Elastic Kubernetes Service Cluster configuration
 #tfsec:ignore:aws-eks-no-public-cluster-access tfsec:ignore:aws-eks-no-public-cluster-access-to-cidr tfsec:ignore:aws-eks-enable-control-plane-logging
+resource "aws_kms_key" "eks" {
+  description             = format("KMS key for %s-eks-cluster secrets", var.name)
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  deletion_window_in_days = 10
+}
+
 resource "aws_eks_cluster" "eks_cluster" {
   name     = format("%s-eks-cluster", var.name)
   role_arn = var.role_arn
   version  = var.cluster_version
+
   vpc_config {
     security_group_ids = [var.security_group_id]
     subnet_ids         = var.subnet_ids
   }
 
   enabled_cluster_log_types = var.eks_cluster_enabled_log_types
+
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = aws_kms_key.eks.arn
+    }
+  }
 
   tags = var.tags
 
@@ -34,14 +48,13 @@ resource "aws_eks_node_group" "eks_node_group" {
     max_size     = var.max_size
     min_size     = var.min_size
   }
-  depends_on = [
-    aws_eks_cluster.eks_cluster
-  ]
 
-  tags = merge(var.tags, {
-    "Name"                                                   = format("%s-eks-node-group", var.name)
-    format("kubernetes.io/cluster/%s-eks-cluster", var.name) = "owned"
-    }
+  tags = merge(
+    var.tags,
+    {
+      "Name"                                                   = format("%s-eks-node-group", var.name)
+      format("kubernetes.io/cluster/%s-eks-cluster", var.name) = "owned"
+    },
   )
 
   labels = {
@@ -52,4 +65,8 @@ resource "aws_eks_node_group" "eks_node_group" {
   lifecycle {
     ignore_changes = [scaling_config[0].desired_size]
   }
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster
+  ]
 }
