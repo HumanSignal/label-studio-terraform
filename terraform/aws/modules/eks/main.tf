8e820100fbcaeaefd83e870b91099dde544f0659
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-04-13 00:05:07 +0400

    Add aws-auth configmap (#21)

--- terraform/aws/modules/eks/main.tf
+++ terraform/aws/modules/eks/main.tf
@@ -74,3 +74,34 @@ resource "aws_eks_node_group" "eks_node_group" {
     aws_eks_cluster.eks_cluster
   ]
 }
+
+# aws-auth configmap
+locals {
+  aws_auth_configmap_data = {
+    mapRoles    = yamlencode(
+        concat(
+            [{
+                rolearn  = var.worker_role_arn
+                username = "system:node:{{EC2PrivateDNSName}}"
+                groups = ["system:bootstrappers","system:nodes"]
+            }],
+            var.aws_auth_roles
+            )
+        )
+    mapUsers    = yamlencode(var.aws_auth_users)
+    mapAccounts = yamlencode(var.aws_auth_accounts)
+  }
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = local.aws_auth_configmap_data
+
+  depends_on = [
+    aws_eks_cluster.eks_cluster,
+  ]
+}
--- terraform/aws/modules/eks/variables.tf
+++ terraform/aws/modules/eks/variables.tf
@@ -123,3 +123,21 @@ variable "persistence_s3_bucket_arn" {
 
 variable "persistence_s3_kms_arn" {
 }
+
+variable "aws_auth_roles" {
+  description = "List of role maps to add to the aws-auth configmap"
+  type        = list(any)
+  default     = []
+}
+
+variable "aws_auth_users" {
+  description = "List of user maps to add to the aws-auth configmap"
+  type        = list(any)
+  default     = []
+}
+
+variable "aws_auth_accounts" {
+  description = "List of account maps to add to the aws-auth configmap"
+  type        = list(any)
+  default     = []
+}
