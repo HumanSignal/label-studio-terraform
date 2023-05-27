af00c1ddaa750d79c3d671afcd7c34d63d3173b3
Sergey Zhuk <sergey.zhuk@heartex.com>
2023-05-27 22:33:58 +0100

    Add DB details to output

--- terraform/aws/env/output.tf
+++ terraform/aws/env/output.tf
@@ -30,3 +30,21 @@ output "load_balancer_host" {
 output "host" {
   value = var.deploy_label_studio ? module.label-studio[0].host : ""
 }
+
+output "redis_host" {
+  value = var.redis_type == "elasticache" && var.enterprise ? "rediss://${module.elasticache[0].host}:${module.elasticache[0].port}/1" : var.redis_host
+}
+
+output "redis_password" {
+  sensitive = true
+  value     = var.redis_type == "elasticache" && var.enterprise ? module.elasticache[0].password : local.redis_password
+}
+
+output "postgresql_host" {
+  value = var.postgresql_type == "rds" ? module.rds[0].host : var.postgresql_host
+}
+
+output "postgresql_password" {
+  sensitive = true
+  value     = var.postgresql_type == "rds" ? module.rds[0].password : local.postgresql_password
+}
